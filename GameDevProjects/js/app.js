458aa674072ab4ec6b7236b145b01e6a7255792e
Chad Conway <agile4java@gmail.com>
2017-09-12 12:19:33 -0500

    Initial Commit

---
+++ GameDevProjects/js/app.js
@@ -0,0 +1,11 @@
+var upperScores = [];
+var lowerScores = [];
+
+function draw(){
+    var canvas = document.getElementById("diceCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(10, 10, 100, 100);
+}
+
